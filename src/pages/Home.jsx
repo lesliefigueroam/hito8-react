189d6d2141e894ad5bc08f4e32172f35e7331c64
Leslie <lesliek.x1999@gmail.com>
2025-10-06 15:01:09 -0300

    Update Home.jsx
    actualizacion del link del server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   useEffect(() => {
     const fetchPizzas = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/pizzas");
+        const response = await fetch("https://api-pizzas-eou9.onrender.com//api/pizzas");
         const data = await response.json();
         setPizzas(data);
       } catch (error) {
